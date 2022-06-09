e4e324b20e47a161004ed2a56235169ce827c4b8
Nikolay Dimov <nikolay.dimov83@gmail.com>
2022-06-09 11:17:43 +0300

    writeLine method ready

--- Functions and Function Expressions/4specialConsole.js
+++ Functions and Function Expressions/4specialConsole.js
@@ -1,11 +1,31 @@
 let specialConsole=(function(){
     function writeLine(textToPrint,placeholder){
-        let arrayOfPlaceholderTexts=writeLine.arguments.shift();
-        let arraySplitByPlaceholders=textToPrint.split(/{[0-9]+}/)
-        for (let i in arraySplitByPlaceholders){
-            arraySplitByPlaceholders[i]+=arrayOfPlaceholderTexts[i];
+        let arrayOfPlaceholderTexts=[]
+        arrayOfPlaceholderTexts=writeLine.arguments;
+        let arrayOfPlaceholderTexts1=[];
+        for (let i in arrayOfPlaceholderTexts){
+            if (i!=='0'){
+                arrayOfPlaceholderTexts1.push(arrayOfPlaceholderTexts[i])
+            }
+            
         }
+        if (textToPrint.search(/{[0-9]+}/)!==-1){
+        let arraySplitByPlaceholders=textToPrint.split(/{[0-9]+}/gi)
+        arraySplitByPlaceholders=arraySplitByPlaceholders.filter(function(x){return x!==""})
+        if(arraySplitByPlaceholders.length!==arrayOfPlaceholderTexts1.length){
+            throw Error('Placeholders in text and place holder arguments in function are different!')
+        }
+        for (let i in arraySplitByPlaceholders){
+            arraySplitByPlaceholders[i]+=arrayOfPlaceholderTexts1[i];
+        }
+        console.log(arraySplitByPlaceholders.join(''));
         return arraySplitByPlaceholders.join('');
+        }
+        else{
+            console.log(textToPrint)
+            return textToPrint
+        } 
+
     }
     return{
         writeLine:writeLine
