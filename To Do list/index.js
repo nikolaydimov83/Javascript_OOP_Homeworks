79843057863ca33cc42b9d4459a3989b94c594d5
Nikolay Dimov <nikolay.dimov83@gmail.com>
2022-06-17 23:07:50 +0300

    dd

--- To Do list/index.js
+++ To Do list/index.js
@@ -2,6 +2,7 @@ let _arrayOfChildren= new WeakMap()
 let _ownSiblingID=new WeakMap()
 let _title= new WeakMap()
 let _divWrapper=new WeakMap()
+let _parent=new WeakMap()
 
 
 class Container{
@@ -14,32 +15,34 @@ constructor(title){
 }
 
 get arrayOfChildren(){
-    return _arrayOfChildren.get(this);
+    return _arrayOfChildren.get(this)
 }
 get title(){
     return _title.get(this)
 }
 set title(value){
-    _title.set(this,value)
+    _title.set(this,value);
 }
 get divWrapper(){
     return _divWrapper.get(this)
 }
 set divWrapper(value){
-    _divWrapper.set(this,value)
+    _divWrapper.set(this,value);
 }
 
-createInitialDivPackage(){
+createDivPackage(){
     let button=document.createElement('button');
     button.addEventListener('click',ev=>{
-        this.addNewElement()
+        this.addNewElement();
     })
     let input=document.createElement('input');
     input.type='text';
-    input.placeholder='Type section Title...'
-    button.innerText='Add Section'
+    input.placeholder='Type section Title...';
+    button.innerText='Add Section';
     let divWrapper=document.createElement('div');
-    divWrapper.innerText=this.title;
+    let containerHeading=document.createElement('h3');
+    containerHeading.innerText=this.title;
+    divWrapper.appendChild(containerHeading);
     divWrapper.appendChild(button);
     divWrapper.appendChild(input);
     divWrapper.id='container';
@@ -50,60 +53,68 @@ createInitialDivPackage(){
     
 }
 addNewElement(){   
-    let newTitle=document.getElementById('container').getElementsByTagName('input')[0].value
-    let newSection= new Section(newTitle,null,this.arrayOfChildren.length)
-    newSection.divWrapper=newSection.createDivPackage()
+    let newTitle=document.getElementById('container').getElementsByTagName('input')[0].value;
+    let newSiblingID=this.arrayOfChildren.length;
+    let newSection= new Section(newTitle,null,newSiblingID);
     this.arrayOfChildren.push(newSection);
-    document.getElementById('container').appendChild(newSection.divWrapper)
+    newSection.divWrapper=newSection.createDivPackage();
+    this.arrayOfChildren=newSection.divWrapper;
+    document.getElementById('container').appendChild(newSection.divWrapper);
     
 }
 assignElementsClass(button,input,divWrapper){
-    button.className=this.constructor.name.toLowerCase()
-    input.className=this.constructor.name.toLowerCase()
-    divWrapper.className=this.constructor.name.toLowerCase()
+    button.className=this.constructor.name.toLowerCase();
+    input.className=this.constructor.name.toLowerCase();
+    divWrapper.className=this.constructor.name.toLowerCase();
 }
 wrapElementsTodiv(button,input,divWrapper){
     divWrapper.appendChild(button);
-    divWrapper.appendChild(input)
+    divWrapper.appendChild(input);
 
 }
 assignNewID(){
-    return `${this.constructor.name}Child-${this.arrayOfChildren.length}`
+    return `${this.constructor.name}Child-${this.ownSiblingID}`;
 }
 assignClass(button,input,divWrapper){
-    button.className=this.constructor.name.toLowerCase()
-    input.className=this.constructor.name.toLowerCase()
-    divWrapper.className=this.constructor.name.toLowerCase()
+    button.className=this.constructor.name.toLowerCase();
+    input.className=this.constructor.name.toLowerCase();
+    divWrapper.className=this.constructor.name.toLowerCase();
 }
 
 }
 class Section extends Container{
-    constructor(title,divWrapper, ownSiblingID){
+    constructor(title,divWrapper,ownSiblingID){
           super(title)
-    _divWrapper.set(this,divWrapper);    
+    _divWrapper.set(this,divWrapper);
+    //let ownSiblingID =this.arrayOfChildren.length;    
     _ownSiblingID.set(this,ownSiblingID);
-    _arrayOfChildren.set(this,[])
+    _arrayOfChildren.set(this,[]);
     }
     get ownSiblingID(){
         return _ownSiblingID.get(this);
     
     }
     get divWrapper(){
-        return _divWrapper.get(this)
+        return _divWrapper.get(this);
     }
     set divWrapper(value){
-        _divWrapper.set(this,value)
+        _divWrapper.set(this,value);
     }
     createDivPackage(){
         let button=document.createElement('button');
+        button.addEventListener('click',ev=>{
+            this.addNewElement();
+        })
         let input=document.createElement('input');
         input.type='text';
-        input.placeholder='Type task...'
-        button.innerText='Add item'
-        let checkBox=document.createElement('input')
+        input.placeholder='Type task...';
+        button.innerText='Add item';
+        let checkBox=document.createElement('input');
         checkBox.type='checkbox';
+        let childTitle=document.createElement('h4');
+        childTitle.innerText=this.title;
         let divWrapper=document.createElement('div');
-    
+        divWrapper.appendChild(childTitle);
         divWrapper.appendChild(button);
         divWrapper.appendChild(input);
         divWrapper.appendChild(checkBox)
@@ -112,17 +123,23 @@ class Section extends Container{
         return divWrapper
     }
     addNewElement(){   
-        let idSection=`${this.constructor.name}Child-${this.ownSiblingID}`
-        let newTitle=document.getElementById(idSection).getElementsByTagName('input')[0].value
-        this.arrayOfChildren.push(new Item(newTitle,divWrapper,this.arrayOfChildren.length));
-        parent=document.getElementById()
+        let idSection=`${this.constructor.name}Child-${this.ownSiblingID}`;
+        let newTitle=document.getElementById(idSection).getElementsByTagName('input')[0].value;
+        let newSiblingID=this.arrayOfChildren.length;
+        let newSection=new Item(newTitle,null,newSiblingID)
+        this.arrayOfChildren.push(newSection);
+        newSection.divWrapper=newSection.createDivPackage();
+        this.arrayOfChildren=newSection.divWrapper;
+        let idHolder=this.assignNewID();
+        document.getElementById(idHolder).appendChild(newSection.divWrapper);
+       
         
     }
     
 }
 
 class Item extends Section{
-    constructor(title,divWrapper, ownSiblingID){
+    constructor(title,divWrapper,ownSiblingID){
         super(title,divWrapper, ownSiblingID)
     }
     
